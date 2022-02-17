e275477da4cedd373015adaf1cd151ca686fa6c3
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-02-17 16:37:30 +0600

    style added

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
 function getInputNumber(inputId){
     const inputText = document.getElementById(inputId);
     const inputNumber = parseFloat(inputText.value);
-    return inputNumber;     
+
+    const numberError = document.getElementById('number-error');
+    const negativeNumberError = document.getElementById('negative-number');
+    if(isNaN(inputNumber)){
+        numberError.style.display ='block';
+    }
+    if(inputNumber < 0){
+      negativeNumberError.style.display = 'block';
+    }
+    if(inputNumber > 0){
+        return inputNumber;
+    }
+    // return inputNumber;     
 }
 
 
@@ -16,9 +28,19 @@ function getTotalExpenses(){
        const totalCost = foodInput + rentInput + clothesInput;
        
        const totalExpenses = document.getElementById('total-expenses');
-       totalExpenses.innerText = totalCost;
 
-       return totalCost;
+       const incomeInput = getInputNumber('income-input');
+       const ifCostMore = document.getElementById('more-cost-error');
+
+       if(incomeInput > totalCost){
+        totalExpenses.innerText = totalCost;
+        return totalCost;
+    }
+      if( totalCost > incomeInput){
+         ifCostMore.style.display = 'block';
+       }
+    
+    //    return totalCost;
 }
 
 function getBalance(){
@@ -26,14 +48,33 @@ function getBalance(){
     const balanceInput = document.getElementById('balance');
     const balance = incomeInput - getTotalExpenses();
     balanceInput.innerText = balance;
+
     return balance;
 }
 //Calculate button event handler
 document.getElementById('calculate-button').addEventListener('click', function(){
-    
+
     getBalance();
 
 })
+
+// //Save button event handler
+// document.getElementById('save-button').addEventListener('click', function(){
+//     const incomeInput =getInputNumber('income-input');
+//     const saveInput = getInputNumber('save-input');
+//     const savingAmount = document.getElementById('saving-amount');
+//     let save = incomeInput * saveInput / 100;
+//     savingAmount.innerText = save;
+    
+//     const balance = getBalance();
+//     //update remaining balance
+//     const remainInput = document.getElementById('remaining-balance');
+//     const remainingBalance = balance -save;
+//     remainInput.innerText = remainingBalance;
+    
+    
+
+// })
 
 //Save button event handler
 document.getElementById('save-button').addEventListener('click', function(){
@@ -41,12 +82,25 @@ document.getElementById('save-button').addEventListener('click', function(){
     const saveInput = getInputNumber('save-input');
     const savingAmount = document.getElementById('saving-amount');
     let save = incomeInput * saveInput / 100;
-    savingAmount.innerText = save;
-    
     const balance = getBalance();
+    const moreSavingsError = document.getElementById('more-savings-error');
+    if(save < balance){
+      savingAmount.innerText = save;
+     const remainInput = document.getElementById('remaining-balance');
+      const remainingBalance = balance -save;
+      remainInput.innerText = remainingBalance;
+    }
+    else{
+      moreSavingsError.style.display ='block';
+    }
+    // savingAmount.innerText = save;
+    
+    // const balance = getBalance();
     //update remaining balance
-    const remainInput = document.getElementById('remaining-balance');
-    const remainingBalance = balance -save;
-    remainInput.innerText = remainingBalance;
+    // const remainInput = document.getElementById('remaining-balance');
+    // const remainingBalance = balance -save;
+    // remainInput.innerText = remainingBalance;
+    
+    
 
 })
